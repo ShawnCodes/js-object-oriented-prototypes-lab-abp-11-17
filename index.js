adadf377acc163704e94618cb27cf0d4f63cc8a4
Shawn T <shawn@huthunt.com>
2018-02-21 02:24:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.veto = function() {
+  BoardMember.prototype.veto = function() {
     return 'No, I must disagree';
   }
   BoardMember.prototype.approve = function() {
